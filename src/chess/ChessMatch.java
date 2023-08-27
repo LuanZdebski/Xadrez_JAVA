0b56a8a2767488029ca90a80951034472ab2fde2
LuanZdebski <luanmatheusbass@hotmail.com>
2023-08-27 13:41:14 -0300

    capture piece system

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import boardgame.Board;
 import boardgame.BoardException;
 import boardgame.Piece;
@@ -12,6 +15,8 @@ public class ChessMatch {
 	private Board board;
 	private Color currentPlayer;
 	private int turn;
+	private List<Piece> piecesOnTheBoard = new ArrayList<>();
+	private List<Piece> capturedPieces = new ArrayList<>();
 
 	public ChessMatch() {
 		board = new Board(8, 8);
@@ -54,6 +59,11 @@ public class ChessMatch {
 		Piece piece = board.removePiece(source);
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(piece, target);
+		if(capturedPiece != null)
+		{
+			piecesOnTheBoard.remove(capturedPiece);
+			capturedPieces.add(capturedPiece);
+		}
 		return capturedPiece;
 	}
 	private void validateSourcePosition(Position pos)
@@ -76,6 +86,7 @@ public class ChessMatch {
 	private void placeNewPiece(char column, int row, ChessPiece piece) 
 	{
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+		piecesOnTheBoard.add(piece);
 	}
 	
 	private void InitialSetup()
