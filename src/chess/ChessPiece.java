6afd0c054da623b5f821c3d151a703c850a979c8
LuanZdebski <luanmatheusbass@hotmail.com>
2023-08-27 14:52:29 -0300

    Counter of movements

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -7,6 +7,7 @@ import boardgame.Position;
 public abstract class ChessPiece extends Piece {
 
 	private Color color;
+	private int moveCount;
 
 	public ChessPiece(Board board, Color color) {
 		super(board);
@@ -17,7 +18,19 @@ public abstract class ChessPiece extends Piece {
 		ChessPiece piece = (ChessPiece) getBoard().piece(pos);
 		return (piece != null && piece.getColor() != color);
 	}
+	public void increaseMoveCount()
+	{
+		moveCount++;
+	}
+	public void decreaseMoveCount()
+	{
+		moveCount--;
+	}
 
+	public int getMoveCount()
+	{
+		return moveCount;
+	}
 	public Color getColor() {
 		return color;
 	}
